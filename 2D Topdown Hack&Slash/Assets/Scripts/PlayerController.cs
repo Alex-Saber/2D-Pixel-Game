49a1ac867eb22c2342de9eba80ee1498dc617db3
Alex Saber <alex@Alexs-MacBook-Pro-4.local>
2018-06-02 13:28:51 -0700

    Updated animation system for characters.

--- 2D Topdown Hack&Slash/Assets/Scripts/PlayerController.cs
+++ 2D Topdown Hack&Slash/Assets/Scripts/PlayerController.cs
@@ -25,11 +25,11 @@ public class PlayerController : NetworkBehaviour
 	public bool is_Attacking;
 	public bool is_Taking_Damage;
 
-	public int left;
+	// Direction is 1 for right and -1 for left.
+	public int direction;
 
 	// Use this for initialization
 	void Start () {
-
 		if (!isLocalPlayer)
 		{
 			Destroy(this);
@@ -48,18 +48,18 @@ public class PlayerController : NetworkBehaviour
 
 		anim_Scale = 1.105f;
 
-		transform.gameObject.name = transform.gameObject.name + " " + (Random.Range(0, 100)).ToString();
 
 		//net_anim = transform.gameObject.GetComponent<NetworkAnimator>();
 		//net_anim.ani = transform.gameObject.GetComponent<Animator>();
 	}
 	
-	public override void OnStartLocalPlayer(){
+	// This is called everytime a new player connects to the server
+	public override void OnStartLocalPlayer() {
 		Debug.Log("Player connected!");	
 		CameraController c = Camera.main.GetComponent<CameraController>();
 		if (c != null)
 			c.target = 	gameObject;
-		}
+	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -74,51 +74,53 @@ public class PlayerController : NetworkBehaviour
 			
 
 			//Checking for Horizontal Movement (Left and Right)
-			if (Input.GetAxisRaw ("Horizontal") > 0) {
-				left = 1;
+			if (Input.GetAxisRaw ("Horizontal") > 0) 
+			{
 				player_RigidBody.velocity = new Vector3 (horizontal_Move_Speed, player_RigidBody.velocity.y, 0f);
 				player_Anim.SetFloat ("Speed", 1f);
-				player_Transform.localScale = new Vector3 (anim_Scale, anim_Scale, 1);
-			} else if (Input.GetAxisRaw ("Horizontal") < 0) {
-				left = -1;
+				player_Anim.SetInteger ("direction", 1);
+				direction = 1;
+			} 
+			else if (Input.GetAxisRaw ("Horizontal") < 0) 
+			{
 				player_RigidBody.velocity = new Vector3 (-horizontal_Move_Speed, player_RigidBody.velocity.y, 0f);
-				player_Anim.SetFloat ("Speed", 1f);
-				player_Transform.localScale = new Vector3 (-anim_Scale, anim_Scale, 1);
-			} else {
+				player_Anim.SetFloat ("Speed", -1f);
+				player_Anim.SetInteger ("direction", -1);
+				direction = -1;
+			} 
+			else 
+			{
 				player_RigidBody.velocity = new Vector3 (0f, player_RigidBody.velocity.y, 0f);
-				player_Transform.localScale = (left == 1) ? new Vector3 (1, 1, 1) : new Vector3 (-1, 1, 1);
 			}
 
 			//Checking for Vertical Movement (Up and Down)
-			if (Input.GetAxisRaw ("Vertical") > 0) {
+			if (Input.GetAxisRaw ("Vertical") > 0) 
+			{
 				player_RigidBody.velocity = new Vector3 (player_RigidBody.velocity.x, vertical_Move_Speed, 0f);
-				player_Anim.SetFloat ("Speed", 1f);
-				player_Transform.localScale = (left == 1) ? new Vector3 (anim_Scale, anim_Scale, 1) : new Vector3 (-anim_Scale, anim_Scale, 1);
-			} else if (Input.GetAxisRaw ("Vertical") < 0) {
+				player_Anim.SetFloat ("Speed", direction);
+			} 
+			else if (Input.GetAxisRaw ("Vertical") < 0) 
+			{
 				player_RigidBody.velocity = new Vector3 (player_RigidBody.velocity.x, -vertical_Move_Speed, 0f);
-				player_Anim.SetFloat ("Speed", 1f);
-				player_Transform.localScale = (left == 1) ? new Vector3 (anim_Scale, anim_Scale, 1) : new Vector3 (-anim_Scale, anim_Scale, 1);
-			} else {
+				player_Anim.SetFloat ("Speed", direction);
+			} 
+			else 
+			{
 				player_RigidBody.velocity = new Vector3 (player_RigidBody.velocity.x, 0f, 0f);
-				if (Input.GetAxisRaw ("Horizontal") == 0) 
-				{
-					player_Transform.localScale = (left == 1) ? new Vector3 (1, 1, 1) : new Vector3 (-1, 1, 1);
-				}
 			}
 
 			//Checking for no Movement
-			if (Input.GetAxisRaw ("Horizontal") == 0f && Input.GetAxisRaw ("Vertical") == 0f) {
+			if (Input.GetAxisRaw ("Horizontal") == 0f && Input.GetAxisRaw ("Vertical") == 0f) 
+			{
 				player_Anim.SetFloat ("Speed", 0f);
 			}
 		}
-		/*****************************
-		 |	  Movement Control End   |
-		 *****************************/
 
 		/***********************
 		 |	   Dash Control    |
 		 ***********************/
-		if (Input.GetKeyDown (KeyCode.Space) && is_Dashing == false) {
+		if (Input.GetKeyDown (KeyCode.Space) && is_Dashing == false) 
+		{
 			is_Dashing = true;
 			player_Anim.SetBool ("Dash", true);
 			player_RigidBody.velocity = Vector3.zero;
@@ -130,16 +132,16 @@ public class PlayerController : NetworkBehaviour
 
 			//Changing character direction based on dash direction
 			//If clicking to the left of the character make him face left
-			if (transform.localScale.x > 0 && (Camera.main.ScreenToWorldPoint (Input.mousePosition) + new Vector3 (0f, 0f, 10f)).x < transform.position.x) {
-				left = -1;
-				transform.localScale = new Vector3 (-1, 1, 1);
-				transform.position = new Vector3 (transform.position.x - 0.08f,transform.position.y,transform.position.z);
+			if (transform.localScale.x > 0 && (Camera.main.ScreenToWorldPoint (Input.mousePosition) + new Vector3 (0f, 0f, 10f)).x < transform.position.x) 
+			{
+				direction = -1;
+				player_Anim.SetInteger("direction", -1);
+				
 			} 
 			//If clicking to the right of the character make him face right
 			else if (transform.localScale.x < 0 && (Camera.main.ScreenToWorldPoint (Input.mousePosition) + new Vector3 (0f, 0f, 10f)).x > transform.position.x) {
-				left = 1;
-				transform.localScale = new Vector3 (1, 1, 1);
-				transform.position = new Vector3 (transform.position.x + 0.08f,transform.position.y,transform.position.z);
+				direction = 1;
+				player_Anim.SetInteger("direction", -1);	
 			}
 		}
 
@@ -147,60 +149,58 @@ public class PlayerController : NetworkBehaviour
 			is_Dashing = false;
 			player_Anim.SetBool ("Dash", false);
 		}
-		/**************************
-		 |	  Dash Control End    |
-		 **************************/
+
 
 
 		/***********************
 		 |	   Slash Control   |
 		 ***********************/
-		if (Input.GetKeyDown (KeyCode.Mouse0) && !is_Dashing && !is_Attacking) {
-			is_Attacking = true;
-			player_Anim.SetBool ("Slash", true);
-			player_RigidBody.velocity = Vector3.zero;
+		if (Input.GetKeyDown (KeyCode.Mouse0) ) {//&& !is_Dashing && !is_Attacking) {
+			//is_Attacking = true;
+			
+			//player_RigidBody.velocity = Vector3.zero;
 
 
 			//Control Player Movement During Slash
-			Vector3 movementDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f,0f,10f)) - player_Transform.position;
-			player_RigidBody.AddForce ((movementDirection/movementDirection.magnitude) * slash_Speed, ForceMode2D.Impulse);
+			Vector3 slashDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f,0f,10f)) - player_Transform.position;
+
+			Debug.Log(player_Transform.position);
+		}
+		/*	player_RigidBody.AddForce ((slashDirection/slashDirection.magnitude) * slash_Speed, ForceMode2D.Impulse);
 
 
 			//Changing character direction
 			//If clicking to the left of the character make him face left
-			if (transform.localScale.x > 0 && (Camera.main.ScreenToWorldPoint (Input.mousePosition) + new Vector3 (0f, 0f, 10f)).x < transform.position.x) {
-				left = 1;
-				transform.localScale = new Vector3 (-1, 1, 1);
-				transform.position = new Vector3 (transform.position.x - 0.08f,transform.position.y,transform.position.z);
-			} 
+			if (slashDirection.x < 0) {
+				direction = -1;
+				player_Anim.SetInteger("direction", -1);
+				player_Anim.SetBool ("Slash", true);
+			}
 			//If clicking to the right of the character make him face right
-			else if (transform.localScale.x < 0 && (Camera.main.ScreenToWorldPoint (Input.mousePosition) + new Vector3 (0f, 0f, 10f)).x > transform.position.x) {
-				left = 0;
-				transform.localScale = new Vector3 (1, 1, 1);
-				transform.position = new Vector3 (transform.position.x + 0.08f,transform.position.y,transform.position.z);
+			else if (slashDirection.x > 0) {
+				direction = 1;
+				player_Anim.SetInteger("direction", 1);
+				player_Anim.SetBool ("Slash", true);
 			}
 
-			player_Transform.localScale = (left == 1) ? new Vector3 (1, 1, 1) : new Vector3 (-1, 1, 1);
 
-			//****************** Spawn Slash Object in direction of mouse click ********************//
-			Vector3 slash_Direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f,0f,10f)) - player_Transform.position;
-			slash_Direction = slash_Direction/(slash_Direction.magnitude * 15);
+			//****************** Spawn Slash Object in direction of mouse click ********************
+			
+			slashDirection = slashDirection/(slashDirection.magnitude * 15);
 
 			GameObject slash = (GameObject)Instantiate(Resources.Load ("Slash_0"));
-			slash.transform.position = player_Transform.position + slash_Direction;
+			slash.transform.position = player_Transform.position + slashDirection;
 			slash.transform.up = slash.transform.position - player_Transform.position;
 		}
-		/**************************
-		 |	  Slash Control End   |
-		 **************************/
+
 
 
 		//If player is currently attacking allow animation to terminate.
-		if (is_Attacking == true && player_Anim.GetCurrentAnimatorStateInfo (0).IsName("Slash 1") && player_Anim.GetCurrentAnimatorStateInfo (0).normalizedTime >= 0.8f) {
+		if (is_Attacking == true && (player_Anim.GetCurrentAnimatorStateInfo (0).IsName("Slash 1 Left") || player_Anim.GetCurrentAnimatorStateInfo (0).IsName("Slash 1 Right")) && player_Anim.GetCurrentAnimatorStateInfo (0).normalizedTime >= 0.8f) {
 			is_Attacking = false;
 			player_Anim.SetBool ("Slash", false);
 		}
-
+		*/
 
 
 		//END OF UPDATE
